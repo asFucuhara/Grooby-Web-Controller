4c055c2c99e95891bd86bede2f5d663e75315b4a
Andre Shindi Fucuhara <asFucu@Shindips-MacBook-Pro.local>
2019-05-10 17:14:45 -0300

    server config

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./controllers/discord');
 app.use('/web/', require('./controllers/web'));
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('prod');
   app.use(express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
